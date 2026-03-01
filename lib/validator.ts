0aa32b9efed02c1eaeb0823909e4756f8e3275f4
mkmaitlo <mkmaitlo0gmail.com>
2026-03-01 17:21:22 +0500

    removed limit

--- lib/validator.ts
+++ lib/validator.ts
@@ -3,7 +3,7 @@ import * as z from "zod"
 
 export const subscriptionFormSchema = z.object({
     title: z.string().min(3,"Title must be atleast 3 characters"),
-    description:z.string().min(10, "Description must be atleast 10 characters").max(400,"Description must be less then 400 characters"),
+    description:z.string().min(10, "Description must be atleast 10 characters"),
     imageUrl:z.string(),
     price: z.string(),
 })
